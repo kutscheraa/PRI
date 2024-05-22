2ea4e209501cdd86560ae0d4c6cb048ba311c3d5
Martin Kučera <kutschera.biz@gmail.com>
2024-05-23 01:51:44 +0200

    del

--- www/html/serve/getGuide.php
+++
@@ -1,8 +0,0 @@
-<?php // přečti a odešli XML soubor s návodem
-
-$guide = @$_GET['guide'];
-$file = "/var/albion/guides/$guide.xml";
-
-header("Content-type: text/xml;");
-if (file_exists($file))
-    readfile($file);
