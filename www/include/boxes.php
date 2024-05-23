c6e4c3d8f95703b84a36171e7414b220a9e33fc9
Martin Kučera <kutschera.biz@gmail.com>
2024-05-24 01:01:08 +0200

    errorBox, successBox h/w fix

--- www/include/boxes.php
+++ www/include/boxes.php
@@ -2,7 +2,7 @@
 // pro zobrazení chyby
 function errorBox($text)
 { ?>
-    <div class="bg-red-900 border border-red-700 text-red-300 m-8 p-4 rounded">
+    <div class="fixed top-14 h-fit w-fit flex justify-center bg-red-900 border border-red-700 text-red-300 m-8 p-4 rounded">
         <?= $text ?>
     </div>
 <?php }
@@ -10,7 +10,7 @@ function errorBox($text)
 // pro zobrazení úspěchu
 function successBox($text)
 { ?>
-    <div class="bg-green-900 border border-green-700 text-green-300 m-8 p-4 rounded">
+    <div class="fixed top-14 h-fit w-fit flex justify-center bg-green-900 border border-green-700 text-green-300 m-8 p-4 rounded">
         <?= $text ?>
     </div>
 <?php }
